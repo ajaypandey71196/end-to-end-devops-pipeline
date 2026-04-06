4eaea5cfb934e016efab6746822944cfeeb8b2b8
ajaypandey71196 <ajay.pandey71196@gmail.com>
2026-04-06 16:08:35 +0530

    new changes in frontend

--- end-to-end-devops-pipeline/app/app.js
+++ end-to-end-devops-pipeline/app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from DevOps Project,   helm ho gya , argocd  testing 🚀");
+  res.send("Hello from DevOps Project,   helm ho gya , argocd  testing  chal rhi hai  🚀");
 });
 
 app.listen(3000, () => {
